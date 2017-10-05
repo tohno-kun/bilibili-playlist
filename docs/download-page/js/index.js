f7197604518a6a01a6ed8ca9929c6255612e2112
toono <443763559@qq.com>
2017-10-05 17:14:45 +0800

    test for download

--- docs/download-page/js/index.js
+++ docs/download-page/js/index.js
@@ -1,8 +1,8 @@
-// 获取安装按钮
-var btnInstall = document.getElementById('btnInstall');
-// 绑定安装事件
-var bindInstallEvent = function() {
-    window.addEventListener(btnInstall, 'click', function(event) {
+$('#install-button').on("click", function() {
+	console.log("is clicked");
+    if (chrome.app.isInstalled) {
+        console.log("已存在");
+    } else {
         chrome.webstore.install("https://chrome.google.com/webstore/detail/fnfkcnlalnhnkdlglnbpdhdacphcnlgd",
             function() {
                 alert("恭喜你，Chrome Extension安装成功");
@@ -10,16 +10,8 @@ var bindInstallEvent = function() {
             function(err) {
                 alert("抱歉，Chrome Extension安装失败");
             });
-    }, false);
-};
-
-// 通过这个API可以直接判断当前浏览器是否已经安装了这个chrome extension
-if (chrome.app.isInstalled) {
-    btnInstall.value = "已安装";
-} else {
-    // 绑定安装事件
-    bindInstallEvent();
-}
+    }
+});
 
 
 var url = "https://raw.githubusercontent.com/tohno-kun/bilibili-playlist/master/lists/";
