aae5336db87e31f6dfd85f5fb1f9e998114e129f
toono <443763559@qq.com>
2017-10-05 16:53:49 +0800

    test for download

--- docs/download-page/js/index.js
+++ docs/download-page/js/index.js
@@ -1,3 +1,27 @@
+// 获取安装按钮
+var btnInstall = document.getElementById('btnInstall');
+// 绑定安装事件
+var bindInstallEvent = function() {
+    window.addEventListener(btnInstall, 'click', function(event) {
+        chrome.webstore.install("https://chrome.google.com/webstore/detail/fnfkcnlalnhnkdlglnbpdhdacphcnlgd",
+            function() {
+                alert("恭喜你，Chrome Extension安装成功");
+            },
+            function(err) {
+                alert("抱歉，Chrome Extension安装失败");
+            });
+    }, false);
+};
+
+// 通过这个API可以直接判断当前浏览器是否已经安装了这个chrome extension
+if (chrome.app.isInstalled) {
+    btnInstall.value = "已安装";
+} else {
+    // 绑定安装事件
+    bindInstallEvent();
+}
+
+
 var url = "https://raw.githubusercontent.com/tohno-kun/bilibili-playlist/master/lists/";
 
 $('#download-json').on("click", "button", function() {
@@ -12,13 +36,13 @@ $('#download-json').on("click", "button", function() {
 
 $.ajax({
     type: "get",
-	url: "https://api.github.com/repos/tohno-kun/bilibili-playlist/contents/lists",
-	success: function(data){
-		var lists = eval(data);
-		for (var i = 0; i < lists.length; i++) {
-			var item = $('<button></button>').text(lists[i]['name'].split('.')[0]);
-			$('#download-json').append(item);
-		}
+    url: "https://api.github.com/repos/tohno-kun/bilibili-playlist/contents/lists",
+    success: function(data) {
+        var lists = eval(data);
+        for (var i = 0; i < lists.length; i++) {
+            var item = $('<button></button>').text(lists[i]['name'].split('.')[0]);
+            $('#download-json').append(item);
+        }
 
-	}
+    }
 });
