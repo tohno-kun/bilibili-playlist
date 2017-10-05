50e2f756c0e1c46a3aecc08819fb6a6847e89e3f
toono <443763559@qq.com>
2017-10-05 22:22:08 +0800

    update doc

--- docs/download-page/js/index.js
+++ docs/download-page/js/index.js
@@ -1,46 +1,47 @@
-(function($){
-  var e = $('#install-button')
-    ,originalText = e.html()
-    ,itemURL = $('link[rel=chrome-webstore-item]').attr('href')
-    ,isChrome = /chrom(e|ium)/.test(navigator.userAgent.toLowerCase());
-  checkAndSetButton();
-  e.click(function(){
-    e.html('正在安装...').attr('disabled',true);
-    chrome.webstore.install(
-      itemURL
-      ,function(){
-        e.html('安装成功！').attr('disabled',true);
-      }
-      ,function(){
-        e.html(originalText).attr('disabled',false);
-      }
-    );
-  });
-  function checkAndSetButton(){
-    if(isChrome){
-      if (typeof chrome !== "undefined" && typeof chrome.app !== "undefined" && chrome.app.isInstalled) {
-        e.html('已安装').attr('disabled',true);
-      } else {
-      	console.log("判断是否安装时失败");
-      	console.log("chrome: " + chrome);
-      	console.log("chrome.app: " + chrome.app);
-      	console.log("chrome.app.isInstalled: " + chrome.app.isInstalled);
-      }
-    }else{
-      e.html('只适用于Chrome浏览器').attr('disabled',true);
-    }
+(function($) {
+    var e = $('#install-button'),
+        originalText = e.html(),
+        itemURL = $('link[rel=chrome-webstore-item]').attr('href'),
+        isChrome = /chrom(e|ium)/.test(navigator.userAgent.toLowerCase());
+    checkAndSetButton();
+    e.click(function() {
+        e.html('正在安装...').attr('disabled', true);
+        chrome.webstore.install(
+            itemURL,
+            function() {
+                e.html('安装成功！').attr('disabled', true);
+            },
+            function() {
+                e.html(originalText).attr('disabled', false);
+            }
+        );
+    });
 
-  }
+    function checkAndSetButton() {
+        if (isChrome) {
+            if (typeof chrome !== "undefined" && typeof chrome.app !== "undefined" && chrome.app.isInstalled) {
+                e.html('已安装').attr('disabled', true);
+            } else {
+                console.log("判断是否安装时失败");
+                console.log("chrome: " + chrome);
+                console.log("chrome.app: " + chrome.app);
+                console.log("chrome.app.isInstalled: " + chrome.app.isInstalled);
+            }
+        } else {
+            e.html('只适用于Chrome浏览器').attr('disabled', true);
+        }
+
+    }
 })(jQuery);
 
 
-$('#clip').on("click", function(){
-	var range = document.createRange();
-	range.selectNode(document.getElementById('clip'));
-	var selection = window.getSelection();
-	if(selection.rangeCount > 0) selection.removeAllRanges();
-	selection.addRange(range);
-	document.execCommand('copy');
+$('#clip').on("click", function() {
+    var range = document.createRange();
+    range.selectNode(document.getElementById('clip'));
+    var selection = window.getSelection();
+    if (selection.rangeCount > 0) selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
 });
 
 var url = "https://raw.githubusercontent.com/tohno-kun/bilibili-playlist/master/lists/";
